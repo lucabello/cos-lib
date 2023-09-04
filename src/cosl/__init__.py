533a3adfdf4089c1087f441ec4725ceeb77c3c78
Simon Aronsson <simme@arcticbit.se>
2023-09-04 10:41:47 +0200

    feat(grafana): add dashboard type for reuse (#11)
    * feat(grafana): add dashboard type for reuse
    
    * feat(grafana): add test for dashboard class

--- src/cosl/__init__.py
+++ src/cosl/__init__.py
@@ -4,6 +4,14 @@
 """Utils for observability Juju charms."""
 
 from .cos_tool import CosTool
+from .grafana_dashboard import GrafanaDashboard
 from .juju_topology import JujuTopology
+from .rules import AlertRules, RecordingRules
 
-__all__ = ["JujuTopology", "CosTool"]
+__all__ = [
+    "JujuTopology",
+    "CosTool",
+    "GrafanaDashboard",
+    "AlertRules",
+    "RecordingRules",
+]
